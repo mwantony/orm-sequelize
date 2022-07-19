6baf7cc9d94018e83622626ea85afc7ebe0d269a
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-19 09:04:53 -0300

    cap 3 vid 3 finalizado

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express()
-
 app.use(bodyParser.json())
-
 const port = 3000
 
-app.get('/teste', (req, res) => {
-  res.status(200).send({mensagem: 'boas-vindas à API'})
-})
+const routes = require('./routes/index')
+routes(app)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
+
+module.exports = app
