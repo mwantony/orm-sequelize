d3c6a1055406f8fb26ab990d8b285ac076491c9e
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-20 12:52:25 -0300

    cap 6 vid 4 finalizado

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -68,11 +68,19 @@ class PessoaController {
       })
       return res.status(200).json(umaMatricula)
     } catch (error) {
-      return res.status(500).json(error.message)
+      return res.status(500).json({erro: error.message})
     }
   }
-
-
+  static async criaMatricula(req, res) {
+    const { estudanteId } = req.params
+    const novaMatricula = { ...req.body, estudante_id: Number(estudanteId) }
+    try {
+      const novaMatriculaCriada = await database.Matriculas.create(novaMatricula)
+      return res.status(200).json(novaMatriculaCriada)
+    } catch (error) {
+      return res.status(500).json({erro: error.message})
+    }
+  }
 
 
 }
