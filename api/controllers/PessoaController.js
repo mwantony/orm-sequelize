0da487140c968b4535b0ee436fda60ed775e062c
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-20 02:59:59 -0300

    cap 4 finalizad

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -16,19 +16,41 @@ class PessoaController {
       const umaPessoa = await database.Pessoas.findOne({
         where: { id: Number(id) },
       });
-      return res.status(200).json(umaPessoa)
+      return res.status(200).json(umaPessoa);
     } catch (error) {
-      return res.status(500).json({erro: error.message})
+      return res.status(500).json({ erro: error.message });
     }
   }
 
   static async criaPessoa(req, res) {
-    const novaPessoa = req.body
+    const novaPessoa = req.body;
     try {
-      const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
-      return res.status(200).json(novaPessoaCriada)
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
     } catch (error) {
-      return res.status(500).json({erro: error.message})
+      return res.status(500).json({ erro: error.message });
+    }
+  }
+
+  static async atualizaPessoa(req, res) {
+    const novasInfos = req.body;
+    const { id } = req.params;
+    try {
+      await database.Pessoas.update(novasInfos, { where: { id: Number(id) } });
+      const pessoaAtualizada = await database.Pessoas.findOne({where: {id: Number(id)}})
+      res.status(200).json(pessoaAtualizada)
+    } catch (error) {
+      res.status(500).json({ erro: error.message });
+    }
+  }
+
+  static async apagaPessoa(req, res) {
+    const {id} = req.params
+    try {
+      await database.Pessoas.destroy({where: {id: Number(id)}})
+      res.status(200).json({mensagem: `pessoa ${id} deletada`})
+    } catch (error) {
+      res.status(500).json({erro: error.message})
     }
   }
 }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -15,4 +15,12 @@ router.post('/pessoas', (req, res) => {
   PessoaController.criaPessoa(req, res)
 })
 
+router.put('/pessoas/:id', (req, res) => {
+  PessoaController.atualizaPessoa(req, res)
+})
+
+router.delete('/pessoas/:id', (req, res) => {
+  PessoaController.apagaPessoa(req, res)
+})
+
 module.exports = router
